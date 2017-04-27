fa1d56a5af27cd928890a55802fd3e4e6f9c9f0c
DESKTOP-VM53LP6\Roland <povilajtis_ra_14@mf.grsu.by>
2017-04-27 10:09:08 +0300

    initial commit

---
+++ LaserExpertise.DAL/Data/Entities/Picture.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace LaserExpertise.DAL.Data.Entities
+{
+    public class Picture
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int PictureID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a picture name")]
+        public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter a description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
+        public string Category { get; set; }
+
+        public byte[] ImageData { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string ImageMimeType { get; set; }
+    }
+}
